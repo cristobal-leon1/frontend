eec782e95d883fe30562a80494b667276d654407
Cristobal Leon <admin@iMac-de-Cristobal.local>
2022-11-08 22:02:41 -0300

    crud cotizaciones

--- frontend/src/pages/IndexPage.vue
+++ frontend/src/pages/IndexPage.vue
@@ -1,17 +1,46 @@
 <template>
-  <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    >
+  <q-page padding>
+    <q-btn @click="access">Ingresar</q-btn>
+    <q-btn @click="access">Crear Cotizacion</q-btn>
+    {{token}} - {{expiresIn}}
   </q-page>
 </template>
 
-<script>
-import { defineComponent } from 'vue'
+<script setup>
+import { api } from 'src/boot/axios';
+import { ref } from 'vue';
 
-export default defineComponent({
-  name: 'IndexPage'
-})
+const token = ref('');
+const expiresIn = ref('');
+
+const access = async () => {
+  try {
+    console.log('me diste click')
+    const res = await api.post('/auth/login', {
+      email: "    pr1sd23@admin.cl",
+      password: "123asd13"
+    })
+    console.log(res.data);
+    token.value = res.data.token;
+    expiresIn.value = res.data.expiresIn;
+  } catch(error) { 
+    console.log(error) 
+  }
+
+  
+}
+
+const refreshToken = async() => {
+  try {
+    const res = await api.get('/auth/refresh');
+    console.log(res.data);
+    token.value = res.data.token;
+    expiresIn.value = res.data.expiresIn;
+  } catch(error) {
+    console.log(error)
+  }
+
+}
+
+refreshToken();
 </script>
