b7d6263622b73fb979733ae650896563211b3f6f
Cristobal Leon <admin@iMac-de-Cristobal.local>
2022-11-09 21:00:21 -0300

    ACTUALIZANDO FRONEND

--- frontend/src/boot/axios.js
+++ frontend/src/boot/axios.js
@@ -2,8 +2,8 @@ import { boot } from 'quasar/wrappers'
 import axios from 'axios'
 
 
-//const api = axios.create({ baseURL: 'http://localhost:5000/api/v1', withCredentials: true })
-const api = axios.create({ baseURL: 'https://backend-zea2.onrender.com/api/v1', withCredentials: true})
+const api = axios.create({ baseURL: 'http://localhost:5000/api/v1', withCredentials: true })
+//const api = axios.create({ baseURL: 'https://backend-zea2.onrender.com/api/v1', withCredentials: true})
 
 
 export default boot(({ app }) => {
