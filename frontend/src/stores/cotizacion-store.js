1c5bf4edbaeb5453d5a788617995cbd5be7f3adf
Cristobal Leon <admin@iMac-de-Cristobal.local>
2022-11-13 15:53:54 -0300

    ACTUALIZANDO FRONEND

---
+++ frontend/src/stores/cotizacion-store.js
@@ -0,0 +1,13 @@
+import { defineStore } from "pinia";
+import { api } from "src/boot/axios";
+import { ref } from "vue";
+import { useUserStore } from "./user-store";
+import { useQuasar } from "quasar";
+
+export const useUserStore = defineStore("cotizacion", () => {
+    
+  
+    return {
+      
+    };
+  });
