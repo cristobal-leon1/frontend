ea703a7dadb852fc8bce2a573a1cadc42e7ffb74
Cristobal Leon <admin@iMac-de-Cristobal.local>
2022-11-16 00:58:07 -0300

    ACTUALIZANDO front end

--- frontend/src/pages/IndexPage.vue
+++ frontend/src/pages/IndexPage.vue
@@ -6,6 +6,50 @@
     <pre>
       {{ cotizacionStore.cotizaciones }}
     </pre>
+    <div class="q-pa-md row items-start q-gutter-md">
+   
+
+
+
+    <q-card flat bordered class="my-card bg-grey-1">
+      <q-card-section>
+        <div class="row items-center no-wrap">
+          <div class="col">
+            <div class="text-h6">CLIENTE COTIZACION</div>
+            <div class="text-subtitle2">  dato  </div>
+            <div class="text-subtitle2">  dato  </div>
+          </div>
+
+          <div class="col-auto">
+            <q-btn color="grey-7" round flat icon="more_vert">
+              <q-menu cover auto-close>
+                <q-list>
+                  <q-item clickable>
+                    <q-item-section>Eliminar cotización</q-item-section>
+                  </q-item>
+                  <q-item clickable>
+                    <q-item-section>Editar Cotización</q-item-section>
+                  </q-item>
+                  
+                </q-list>
+              </q-menu>
+            </q-btn>
+          </div>
+        </div>
+      </q-card-section>
+
+      <q-card-section>
+        {{ lorem }}
+      </q-card-section>
+
+      <q-separator />
+
+      <q-card-actions>
+        <q-btn flat>Marcar como vendida</q-btn>
+      </q-card-actions>
+    </q-card>
+  </div>
+    
   </q-page>
 </template>
 
